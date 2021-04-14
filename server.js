6553b19d6b44c9763d29814fa87a76cd2f40aafd
pesjo <pernillasjoeberg@hotmail.se>
2021-04-14 10:05:04 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var mysql = require("mysql2");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+const port = 5000;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "annonssystem",
+});
+
+connection.connect();
+
+
+console.log("hej");
+
+app.listen(port, () => {
+    console.log(`app listening at port http://localhost:${port}`);
+    console.log({ port });
+  });
