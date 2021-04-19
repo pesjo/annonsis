c18402c7b63f7f59255318b3d9cfcd3e02cec8a0
pesjo <pernillasjoeberg@hotmail.se>
2021-04-19 13:02:35 +0200

    Ett dåligt försök på create Ad

--- frontapp/src/components/AnnonsForm/index.jsx
+++ frontapp/src/components/AnnonsForm/index.jsx
@@ -2,51 +2,51 @@ import React, { useState } from "react";
 import useStyles from "./styles";
 import TextInput from "../TextInput";
 import {
-    postAnnons
-  } from "../../api/prenumerantApi";
+  postAnnons
+} from "../../api/annonsApi";
 
 const AnnonsForm = () => {
-    const [varupris, set_varupris] = useState("");
-    const [innehall, set_innehall] = useState("");
-    const [rubrik, set_rubrik] = useState("");
-    const [annonspris, set_annonspris] = useState("");
-    const classes = useStyles();
+  const [varupris, set_varupris] = useState("");
+  const [innehall, set_innehall] = useState("");
+  const [rubrik, set_rubrik] = useState("");
+  const [annonspris, set_annonspris] = useState("");
+  const classes = useStyles();
 
-    const create_ad = async (e) => {
-        e.preventDefault();
+  const create_ad = async (e) => {
+    e.preventDefault();
+    console.log("Inne i create_ad");
     
-        console.log("Inne i create_ad");
-        const data = {
-          ad_varupris: varupris,
-          ad_innehall: innehall,
-          ad_rubrik: rubrik,
-          ad_annonspris: annonspris
-        };
-    
-        const response = await postAnnons(data);
-        console.log(response);
-      };
+    const data = {
+      ad_varupris: varupris,
+      ad_innehall: innehall,
+      ad_rubrik: rubrik,
+      ad_annonspris: annonspris
+    };
+    console.log("data i create_ad", data)
+    const response = await postAnnons(data);
+    console.log("response i create_ad", response);
+  };
 
-    return (
-        <>
-            <h2>Lägg ut annons</h2>
+  return (
+    <>
+      <h2>Lägg ut annons</h2>
 
-            <form className="CreateAd" onSubmit={create_ad}>
+      <form className="CreateAd" onSubmit={create_ad}>
 
-                <TextInput label="Varupris" setValue ={set_varupris}></TextInput>
-                <br />
-                <TextInput label="Innehåll " setValue ={set_innehall}></TextInput>
-                <br />
-                <TextInput label="Rubrik" setValue ={set_rubrik}></TextInput>
-                <br />
-                <TextInput label="Annonspris" setValue ={set_annonspris}></TextInput>
-                <br />
+        <TextInput label="Varupris" setValue={set_varupris}></TextInput>
+        <br />
+        <TextInput label="Innehåll " setValue={set_innehall}></TextInput>
+        <br />
+        <TextInput label="Rubrik" setValue={set_rubrik}></TextInput>
+        <br />
+        <TextInput label="Annonspris" setValue={set_annonspris}></TextInput>
+        <br />
 
-                <input type="submit" value="Lägg ut annons" className={classes.buttonStyle}></input>
-            </form>
+        <input type="submit" value="Lägg ut annons" className={classes.buttonStyle}></input>
+      </form>
 
-        </>
-    );
+    </>
+  );
 };
 
 export default AnnonsForm;
--- server.js
+++ server.js
@@ -11,16 +11,16 @@ app.use(express.json());
 const port = 5000;
 
 var db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "annonssystem",
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "annonssystem",
 });
 db.connect();
 
 app.listen(port, () => {
-      console.log(`app listening at port http://localhost:${port}`);
-      console.log({ port });
+  console.log(`app listening at port http://localhost:${port}`);
+  console.log({ port });
 });
 
 console.log("hit kmr den 1")
@@ -30,20 +30,20 @@ app.get("/annonsApi/annonsorget/:id", (req, res) => {
   const organisationsnummer = req.params.id;
   console.log("organisationsnummer: ", organisationsnummer)
 
-   const sqlSelect = `Select * FROM tbl_annonsorer WHERE annonsor_orgnr = ?`;
-   console.log("ökhvl")
-   db.query(sqlSelect, [organisationsnummer], (err, result) => {
-      if(result){
-           console.log(result);
-           res.json(result[0]);
-           console.log(result[0])
-      }
-      else{
-        console.log(err)
-       }
+  const sqlSelect = `Select * FROM tbl_annonsorer WHERE annonsor_orgnr = ?`;
+  console.log("ökhvl")
+  db.query(sqlSelect, [organisationsnummer], (err, result) => {
+    if (result) {
+      console.log(result);
+      res.json(result[0]);
+      console.log(result[0])
+    }
+    else {
+      console.log(err)
+    }
 
-   return result;
-});
+    return result;
+  });
 });
 console.log("hit kmr den 2")
 
@@ -69,35 +69,35 @@ console.log("hit kmr den 2")
 console.log("hit kmr den 3")
 
 
-app.get("/annonsApi/annonspost", (req, res) => {
-     const varupris = 2;
-     const innehall = "1jjjjj";
-     const rubrik = "1";
-     const annonspris = 1;
+app.post("http://localhost:5000/annonsApi/annonspost", (req, res) => {
+  console.log("kommer till seervern");
+  const varupris = req.body.ad_varupris;
+  const innehall = req.body.innehall;
+  const rubrik = req.body.rubrik;
+  const annonspris = req.body.annonspris;
 
-     const sqlSelect = `INSERT INTO tbl_ads SET ad_varupris = ?, ad_innehall = ?, ad_rubrik = ?, ad_annonspris = ?;`;
-
-     connection.query(sqlSelect, [varupris, innehall, rubrik, annonspris], (err, result) => {
-       if(result){
-            console.log(result);
-            res.status(200).send("");
-
-
-       }
-       else{
-         console.log(err)
-
-       }
-   });
-
-   
+  const sqlSelect = `INSERT INTO tbl_ads SET ad_varupris = ?, ad_innehall = ?, ad_rubrik = ?, ad_annonspris = ?;`;
+  console.log("I SERVERN")
+  db.query(sqlSelect, [varupris, innehall, rubrik, annonspris], (err, result) => {
+    if (result) {
+      console.log(result);
+      res.status(200).send("");
 
 
-app.listen(port, () => {
-        console.log(`app listening at port http://localhost:${port}`);
-        console.log({ port });
-      });
-    
- });
+    }
+    else {
+      console.log(err)
 
- 
+    }
+  });
+
+
+  console.log("hit kmr den ")
+
+  app.listen(port, () => {
+    console.log(`app listening at port http://localhost:${port}`);
+    console.log({ port });
+  });
+
+});
+
